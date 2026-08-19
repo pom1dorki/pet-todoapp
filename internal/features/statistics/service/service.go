a6225f61ae578c68c824ee4f2b9184d367513f38
Ilya Petrischev <pom1dorki@yandex.ru>
2026-08-19 10:54:26 +0300

    feature/statistics

---
+++ internal/features/statistics/service/service.go
@@ -0,0 +1,22 @@
+package statistics_service
+
+import (
+	"context"
+	"time"
+
+	"github.com/pom1dorki/pet-todoapp/internal/core/domain"
+)
+
+type StatisticsService struct {
+	statisticsRepository StatisticsRepository
+}
+
+type StatisticsRepository interface {
+	GetTasks(ctx context.Context, userID *int, from *time.Time, to *time.Time) ([]domain.Task, error)
+}
+
+func NewStatisticsService(statisticsRepository StatisticsRepository) *StatisticsService {
+	return &StatisticsService{
+		statisticsRepository: statisticsRepository,
+	}
+}
